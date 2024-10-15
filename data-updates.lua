283856607bce5dcfecf2fa3a3b48c9555ba96691
Alkumist <ponyyouman@gmail.com>
2024-10-15 09:32:24 -0600

    Initial commit

---
+++ data-updates.lua
@@ -0,0 +1,32 @@
+data.raw.recipe["im-intermediate-module-1"].ingredients = {
+  {"electronic-circuit", 5},
+  {"advanced-circuit", 5}
+}
+data.raw.recipe["im-intermediate-module-2"].ingredients = {
+  {"advanced-circuit", 5},
+  {"processing-unit", 5},
+  {"im-intermediate-module-1", 4},
+}
+data.raw.recipe["im-intermediate-module-3"].ingredients = {
+  {"advanced-circuit", 5},
+  {"processing-unit", 5},
+  {"im-intermediate-module-2", 5}
+}
+data.raw.recipe["speed-module-2"].ingredients = {
+  {"im-intermediate-module-2", 1}
+}
+data.raw.recipe["speed-module-3"].ingredients = {
+  {"im-intermediate-module-3", 1}
+}
+data.raw.recipe["effectivity-module-2"].ingredients = {
+  {"im-intermediate-module-2", 1}
+}
+data.raw.recipe["effectivity-module-3"].ingredients = {
+  {"im-intermediate-module-3", 1}
+}
+data.raw.recipe["productivity-module-2"].ingredients = {
+  {"im-intermediate-module-2", 1}
+}
+data.raw.recipe["productivity-module-3"].ingredients = {
+  {"im-intermediate-module-3", 1}
+}
